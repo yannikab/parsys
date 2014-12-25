10cc31ba845fd6375064e971290bb6dea00ee14e
Yanni Kabilafkas <yanni.kab@gmail.com>
2014-12-25 07:03:41 +0200

    Parallel algorithm properly distributes data to slave processes.

--- par_conv/main.c
+++ par_conv/main.c
@@ -136,7 +136,7 @@ bool read_image(unsigned char ***input_buffer)
 
 	if (!ok)
 		dealloc_uchar_array(input_buffer);
-	
+
 	return ok;
 }
 
@@ -217,173 +217,148 @@ int main(int argc, char** argv)
 	int size, rank;
 	int i, j, c;
 
-	//	MPI_Init(&argc, &argv);
-	//
-	//	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	//	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	//
-	size = 2;
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	int total_area = WIDTH * HEIGHT;
 	int local_area = total_area / (size - 1);
-	//
+
 	int s = 1;
 	while (s * s * local_area != total_area)
 		s++;
 
-
-	//	printf("%d\n", s);
-
 	int local_width = WIDTH / s;
 	int local_height = HEIGHT / s;
 
-	printf("%d %d\n", local_width, local_height);
+	//	printf("%d %d\n", local_width, local_height);
 
-	//	if (rank == 0)
-	//	{
-	unsigned char (**input_buffer)[CHANNELS]; // one buffer of arrays of 3 bytes
-
-	/* Read input image. */
-
-	if (read_image((unsigned char ***) &input_buffer))
-		printf("OK\n");
-
-	//	alloc_uchar_array((unsigned char ***) &input_buffer, HEIGHT, WIDTH, CHANNELS);
-
-	unsigned char **output_buffer[CHANNELS];
-
-	for (c = 0; c < CHANNELS; c++)
-		alloc_uchar_array(&(output_buffer[c]), HEIGHT, WIDTH, 1);
-
-	for (c = 0; c < CHANNELS; c++)
-		for (i = 0; i < HEIGHT; i++)
-			for (j = 0; j < WIDTH; j++)
-				output_buffer[c][i][j] = input_buffer[i][j][c];
-
-	for (c = 0; c < CHANNELS; c++)
+	if (rank == 0)
 	{
-		out_filename[c] = NULL;
+		unsigned char (**input_buffer)[CHANNELS];
 
-		out_filename[c] = malloc(STRSIZE * sizeof (char));
+		/* Read input image. */
 
-		if (out_filename[c] == NULL)
+		read_image((unsigned char ***) &input_buffer);
+
+		/* Create "subarray" datatype. */
+
+		MPI_Datatype local_image_t;
+		MPI_Type_vector(local_height, local_width * CHANNELS, WIDTH * CHANNELS, MPI_UNSIGNED_CHAR, &local_image_t);
+		MPI_Type_commit(&local_image_t);
+
+		/* Send each subarray to corresponding process. */
+
+		printf("Master: %d \n", rank);
+
+		for (i = 1; i < size; i++)
 		{
-			perror("malloc");
-			for (i = 0; i < c; i++)
-				free(out_filename[c]);
-			return (EXIT_FAILURE);
+			int x = (i - 1) % s;
+			int y = (i - 1) / s;
+			MPI_Send(&(input_buffer[y * local_height][x * local_width][0]), 1, local_image_t, i, 0, MPI_COMM_WORLD);
+			//			int d;
+			//			MPI_Send(&d, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		}
+	} else
+	{
+		unsigned char (**local_buffer)[CHANNELS];
+		alloc_uchar_array((unsigned char ***) &local_buffer, local_height, local_width, CHANNELS);
+
+		printf("Slave: %d \n", rank);
+		MPI_Status status;
+		MPI_Recv(&(local_buffer[0][0][0]), local_height * local_width * CHANNELS, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+		//		int d;
+		//		MPI_Recv(&d, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		printf("Received at %d.\n", rank);
+		int received;
+		MPI_Get_count(&status, MPI_FLOAT, &received);
+		printf("Count: %d.\n", received);
+
+		unsigned char **output_buffer[CHANNELS];
+
+		for (c = 0; c < CHANNELS; c++)
+			alloc_uchar_array(&(output_buffer[c]), local_height, local_width, 1);
+
+		for (c = 0; c < CHANNELS; c++)
+			for (i = 0; i < local_height; i++)
+				for (j = 0; j < local_width; j++)
+					output_buffer[c][i][j] = local_buffer[i][j][c];
+
+		for (c = 0; c < CHANNELS; c++)
+		{
+			out_filename[c] = NULL;
+
+			out_filename[c] = malloc(STRSIZE * sizeof (char));
+
+			if (out_filename[c] == NULL)
+			{
+				perror("malloc");
+				for (i = 0; i < c; i++)
+					free(out_filename[c]);
+				return (EXIT_FAILURE);
+			}
+
+			out_filename[c][0] = '\0';
+
+			char s[STRSIZE];
+
+			s[0] = '\0';
+			sprintf(s, "../%d", c);
+			strcat(out_filename[c], s);
+			strcat(out_filename[c], "_");
+
+			s[0] = '\0';
+			sprintf(s, "%d", rank - 1);
+			strcat(out_filename[c], s);
+			strcat(out_filename[c], "_");
+
+			strcat(out_filename[c], OUTFILENAME);
 		}
 
-		out_filename[c][0] = '\0';
-
-		char s[STRSIZE];
-		s[0] = '\0';
-		sprintf(s, "../%d", c);
-		strcat(out_filename[c], s);
-
-		strcat(out_filename[c], "_");
-
-		strcat(out_filename[c], OUTFILENAME);
-	}
-
-	for (c = 0; c < CHANNELS; c++)
-	{
-		out_fp[c] = NULL;
-		out_fp[c] = fopen(out_filename[c], "wb");
-		if (out_fp[c] == NULL)
+		for (c = 0; c < CHANNELS; c++)
 		{
-			perror(out_filename[c]);
-			return (EXIT_FAILURE);
+			out_fp[c] = NULL;
+			out_fp[c] = fopen(out_filename[c], "wb");
+			if (out_fp[c] == NULL)
+			{
+				perror(out_filename[c]);
+				return (EXIT_FAILURE);
+			}
+		}
+
+		for (c = 0; c < CHANNELS; c++)
+			for (i = 0; i < local_height; i++)
+				fwrite(output_buffer[c][i], 1, local_width, out_fp[c]);
+
+		for (c = 0; c < CHANNELS; c++)
+		{
+			if (fclose(out_fp[c]))
+			{
+				fprintf(stderr, "Could not close file: %s", out_filename[c]);
+				return (EXIT_FAILURE);
+			}
+		}
+
+		/* Convert output files to tiff format. */
+
+		char command[STRSIZE];
+		for (c = 0; c < CHANNELS; c++)
+		{
+			command[0] = '\0';
+			sprintf(command, "raw2tiff -l %d -w %d %s %s.tiff", local_height, local_width, out_filename[c], out_filename[c]);
+			printf("%s\n", command);
+			system(command);
+		}
+
+		for (c = 0; c < CHANNELS; c++)
+		{
+			free(out_filename[c]);
+			out_filename[c] = NULL;
 		}
 	}
 
-	for (c = 0; c < CHANNELS; c++)
-		for (i = 0; i < HEIGHT; i++)
-			fwrite(output_buffer[c][i], 1, WIDTH, out_fp[c]);
-
-	for (c = 0; c < CHANNELS; c++)
-	{
-		if (fclose(out_fp[c]))
-		{
-			fprintf(stderr, "Could not close file: %s", out_filename[c]);
-			return (EXIT_FAILURE);
-		}
-	}
-
-	//		/* Create "subarray" datatype. */
-	//
-	//		MPI_Datatype local_image_t;
-	//		MPI_Type_vector(local_height, local_width * CHANNELS, WIDTH * CHANNELS, MPI_UNSIGNED_CHAR, &local_image_t);
-	//		MPI_Type_commit(&local_image_t);
-	//
-	//		/* Send each subarray to corresponding process. */
-	//
-	//		printf("Master: %d \n", rank);
-	//
-	//		for (i = 1; i < size; i++)
-	//		{
-	//			int x = (i - 1) % s;
-	//			int y = (i - 1) / s;
-	//			MPI_Send(&(input_buffer[y * local_height][x * local_width]), 1, local_image_t, i, 0, MPI_COMM_WORLD);
-	//			//			int d;
-	//			//			MPI_Send(&d, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-	//		}
-
-	//		MPI_Finalize();
-
-	//	} else
-	//	{
-	//		unsigned char (**local_buffer)[CHANNELS];
-	//		alloc_uchar_array((unsigned char ***) &local_buffer, local_height, local_width, CHANNELS);
-	//
-	//		printf("Slave: %d \n", rank);
-	//		MPI_Status status;
-	//		MPI_Recv(local_buffer, local_height * local_width * CHANNELS, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-	//		//		int d;
-	//		//		MPI_Recv(&d, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-	//		printf("Received at %d.\n", rank);
-	//		int received;
-	//		MPI_Get_count(&status, MPI_FLOAT, &received);
-	//		printf("Count: %d.\n", received);
-	//
-	//	MPI_Finalize();
-
-	//		unsigned char (**output_channel)[local_width];
-	//
-	//		for (c = 0; c < CHANNELS; c++)
-	//		{
-	//			output_channel[c] = NULL;
-	//			output_channel[c] = malloc(local_height * sizeof (*output_channel[c]));
-	//			if (output_channel[c] == NULL)
-	//			{
-	//				perror("malloc");
-	//				return (EXIT_FAILURE);
-	//			}
-	//		}
-	//
-	//		for (i = 0; i < local_height; i++)
-	//			for (j = 0; j < local_width; j++)
-	//				for (c = 0; c < CHANNELS; c++)
-	//					output_channel[c][i][j] = (char) local_buffer[i][j][c];
-	//
-	//		open_output_files(rank);
-	//
-	//		/* Write out channels to separate files. */
-	//
-	//		for (c = 0; c < CHANNELS; c++)
-	//			for (i = 0; i < local_height; i++)
-	//				fwrite(output_channel[c][i], 1, local_width, out_fp[c]);
-	//
-	//		close_output_files();
-	//
-	/* Convert output files to tiff format. */
-
-	char command[STRSIZE];
-	for (c = 0; c < CHANNELS; c++)
-	{
-		command[0] = '\0';
-		sprintf(command, "raw2tiff -l %d -w %d %s %s.tiff", local_height, local_width, out_filename[c], out_filename[c]);
-		system(command);
-	}
+	MPI_Finalize();
 
 	return (EXIT_SUCCESS);
 }
