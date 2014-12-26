2e130b3ad3acaa32e9f74c4fc0562352570e68b8
Yanni Kabilafkas <yanni.kab@gmail.com>
2014-12-26 04:48:58 +0200

    Inner and outer filter added, they seem to work correctly for B=1.

--- par_conv/main.c
+++ par_conv/main.c
@@ -18,6 +18,11 @@
 #define HEIGHT 2520
 #define CHANNELS 3
 
+#define B 1
+static const float filter[3][3] = {0.0625f, 0.125f, 0.0625f, 0.125f, 0.25f, 0.125f, 0.0625f, 0.125f, 0.0625f};
+
+#define ITERATIONS 20
+
 static FILE *out_fp[CHANNELS];
 
 static char *out_filename[CHANNELS];
@@ -209,18 +214,116 @@ bool close_output_files()
 	return true;
 }
 
+void apply_inner_filter(float (**output_image)[CHANNELS], float (**input_image)[CHANNELS], int height, int width)
+{
+	unsigned int i, j, c;
+	int p, q;
+
+	for (i = 2 * B; i < height - 2 * B; i++)
+	{
+		for (j = 2 * B; j < width - 2 * B; j++)
+		{
+			for (c = 0; c < CHANNELS; c++)
+			{
+				float value = 0.0f;
+
+				for (p = -B; p <= B; p++)
+					for (q = -B; q <= B; q++)
+						value += input_image[i - p][j - q][c] * filter[p + B][q + B];
+
+				output_image[i][j][c] = value;
+			}
+		}
+	}
+}
+
+void apply_outer_filter(float (**output_image)[CHANNELS], float (**input_image)[CHANNELS], int height, int width)
+{
+	unsigned int i, j, c;
+	int p, q;
+
+	for (c = 0; c < CHANNELS; c++)
+	{
+		/* Left border column. */
+
+		for (i = B; i < height - B; i++)
+		{
+			for (j = B; j < 2 * B; j++)
+			{
+				float value = 0.0f;
+
+				for (p = -B; p <= B; p++)
+					for (q = -B; q <= B; q++)
+						value += input_image[i - p][j - q][c] * filter[p + B][q + B];
+
+				output_image[i][j][c] = value;
+			}
+		}
+
+		/* Right border column. */
+
+		for (i = B; i < height - B; i++)
+		{
+			for (j = width - 1 - B; j > width - 1 - 2 * B; j--)
+			{
+				float value = 0.0f;
+
+				for (p = -B; p <= B; p++)
+					for (q = -B; q <= B; q++)
+						value += input_image[i - p][j - q][c] * filter[p + B][q + B];
+
+				output_image[i][j][c] = value;
+			}
+		}
+
+		/* Top border row, avoid recalculating overlap with columns. */
+
+		for (j = 2 * B; j < width - 2 * B; j++)
+		{
+			for (i = B; i < 2 * B; i++)
+			{
+				float value = 0.0f;
+
+				for (p = -B; p <= B; p++)
+					for (q = -B; q <= B; q++)
+						value += input_image[i - p][j - q][c] * filter[p + B][q + B];
+
+				output_image[i][j][c] = value;
+			}
+		}
+
+		/* Bottom border row, avoid recalculating overlap with columns. */
+
+		for (j = 2 * B; j < width - 2 * B; j++)
+		{
+			for (i = height - 1 - B; i > height - 1 - 2 * B; i--)
+			{
+				float value = 0.0f;
+
+				for (p = -B; p <= B; p++)
+					for (q = -B; q <= B; q++)
+						value += input_image[i - p][j - q][c] * filter[p + B][q + B];
+
+				output_image[i][j][c] = value;
+			}
+		}
+	}
+}
+
 /*
  * 
  */
 int main(int argc, char** argv)
 {
-	int size, rank;
-	int i, j, c;
+	int size, rank, master;
+	int i, j, c, r;
 
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	master = size - 1;
 
 	int total_area = WIDTH * HEIGHT;
 	int local_area = total_area / (size - 1);
@@ -234,7 +337,15 @@ int main(int argc, char** argv)
 
 	//	printf("%d %d\n", local_width, local_height);
 
-	if (rank == 0)
+	/* Define s x s cartesian topology of slaves, excluding master node which is last. */
+
+	//	MPI_Comm comm_slaves;
+	//	int ndims = 2;
+	//	int dims[] = {s, s};
+	//	int periods[] = {0, 0};
+	//	MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, true, &comm_slaves);
+
+	if (rank == master)
 	{
 		unsigned char (**input_buffer)[CHANNELS];
 
@@ -242,38 +353,85 @@ int main(int argc, char** argv)
 
 		read_image((unsigned char ***) &input_buffer);
 
+		/* Convert byte data to float for applying arithmetic operations. */
+
+		float (**input_image_data)[CHANNELS];
+
+		alloc_float_array((float ***) &input_image_data, HEIGHT, WIDTH, CHANNELS);
+
+		for (c = 0; c < CHANNELS; c++)
+			for (i = 0; i < HEIGHT; i++)
+				for (j = 0; j < WIDTH; j++)
+					input_image_data[i][j][c] = (float) input_buffer[i][j][c];
+
 		/* Create "subarray" datatype. */
 
 		MPI_Datatype local_image_t;
-		MPI_Type_vector(local_height, local_width * CHANNELS, WIDTH * CHANNELS, MPI_UNSIGNED_CHAR, &local_image_t);
+		MPI_Type_vector(local_height, local_width * CHANNELS, WIDTH * CHANNELS, MPI_FLOAT, &local_image_t);
 		MPI_Type_commit(&local_image_t);
 
 		/* Send each subarray to corresponding process. */
 
 		printf("Master: %d \n", rank);
 
-		for (i = 1; i < size; i++)
+		for (r = 0; r < size; r++)
 		{
-			int x = (i - 1) % s;
-			int y = (i - 1) / s;
-			MPI_Send(&(input_buffer[y * local_height][x * local_width][0]), 1, local_image_t, i, 0, MPI_COMM_WORLD);
+			if (r == master)
+				continue;
+
 			//			int d;
-			//			MPI_Send(&d, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			//			MPI_Send(&d, 1, MPI_INT, r, 0, MPI_COMM_WORLD);
+			int x = r % s;
+			int y = r / s;
+			MPI_Send(&(input_image_data[y * local_height][x * local_width][0]), 1, local_image_t, r, 0, MPI_COMM_WORLD);
+
+			printf("Send returned: %d \n", rank);
 		}
 	} else
 	{
-		unsigned char (**local_buffer)[CHANNELS];
-		alloc_uchar_array((unsigned char ***) &local_buffer, local_height, local_width, CHANNELS);
+		float (**input_image_data)[CHANNELS];
+		alloc_float_array((float ***) &input_image_data, B + local_height + B, B + local_width + B, CHANNELS);
+
+		float (**output_image_data)[CHANNELS];
+		alloc_float_array((float ***) &output_image_data, B + local_height + B, B + local_width + B, CHANNELS);
+
+		/* Create inner image datatype. */
+
+		MPI_Datatype inner_image_t;
+		MPI_Type_vector(local_height, local_width * CHANNELS, (B + local_width + B) * CHANNELS, MPI_FLOAT, &inner_image_t);
+		MPI_Type_commit(&inner_image_t);
+
+		/* Receive inner image. */
 
 		printf("Slave: %d \n", rank);
 		MPI_Status status;
-		MPI_Recv(&(local_buffer[0][0][0]), local_height * local_width * CHANNELS, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD, &status);
 		//		int d;
-		//		MPI_Recv(&d, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		//		MPI_Recv(&d, 1, MPI_INT, master, 0, MPI_COMM_WORLD, &status);
+		//		MPI_Recv(&(output_image_data[1][1][0]), local_height * local_width * CHANNELS, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&(output_image_data[B][B][0]), 1, inner_image_t, master, 0, MPI_COMM_WORLD, &status);
 		printf("Received at %d.\n", rank);
+
 		int received;
-		MPI_Get_count(&status, MPI_FLOAT, &received);
-		printf("Count: %d.\n", received);
+		MPI_Get_count(&status, inner_image_t, &received);
+		//		printf("Count: %d.\n", received);
+
+		/* Apply filter. */
+
+		for (i = 0; i < ITERATIONS; i++)
+		{
+			/* Use previous output as new input. */
+
+			float (**tmp)[CHANNELS];
+			tmp = input_image_data;
+			input_image_data = output_image_data;
+			output_image_data = tmp;
+
+			// TODO: send and receive neighbouring node data
+
+			apply_inner_filter(output_image_data, input_image_data, B + local_height + B, B + local_width + B);
+
+			apply_outer_filter(output_image_data, input_image_data, B + local_height + B, B + local_width + B);
+		}
 
 		unsigned char **output_buffer[CHANNELS];
 
@@ -283,7 +441,7 @@ int main(int argc, char** argv)
 		for (c = 0; c < CHANNELS; c++)
 			for (i = 0; i < local_height; i++)
 				for (j = 0; j < local_width; j++)
-					output_buffer[c][i][j] = local_buffer[i][j][c];
+					output_buffer[c][i][j] = (char) output_image_data[i + B][j + B][c];
 
 		for (c = 0; c < CHANNELS; c++)
 		{
@@ -309,7 +467,7 @@ int main(int argc, char** argv)
 			strcat(out_filename[c], "_");
 
 			s[0] = '\0';
-			sprintf(s, "%d", rank - 1);
+			sprintf(s, "%d", rank);
 			strcat(out_filename[c], s);
 			strcat(out_filename[c], "_");
 
