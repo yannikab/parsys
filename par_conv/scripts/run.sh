fa4e4e4de5dbb1b645d75c236424ab3009fedf28
Yanni Kabilafkas <yanni.kab@gmail.com>
2015-02-15 07:57:04 +0200

    Script changes.

--- par_conv/scripts/run.sh
+++ par_conv/scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # mpich2
-mpiexec -machinefile $1 -np `expr $4 \* $5 + 1` ./dist/Release/GNU-Linux-x86/par_conv $2 $3 $4 $5
+mpiexec -machinefile $1 -np `expr $4 \* $5 + 1` ../dist/Release/GNU-Linux-x86/par_conv $2 $3 $4 $5
